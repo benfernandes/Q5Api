2dd31650494213e28f1ef6f6187b414f699bf033
Ben Fernandes <benjamin.fernandes@softwire.com>
2021-09-21 01:31:49 +0100

    Updated Queue model annotations and created migrations

--- Models/Queue.cs
+++ Models/Queue.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Q5Api.Models
 {
     public class Queue
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
