e01bd04e1719ba9745c091737b4e7cc58b1d4434
Ben Fernandes <benjamin.fernandes@softwire.com>
2021-09-21 23:37:33 +0100

    Added data annotation to QueueCreateDto

--- Dtos/QueueCreateDto.cs
+++ Dtos/QueueCreateDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Q5Api.Dtos
 {
     public class QueueCreateDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
